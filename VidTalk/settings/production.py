7757d0d5063a84ef6af50d3910e299733ab29391
Zachary Cherney <zach@Zacharys-MacBook-Pro.local>
2024-09-14 18:02:36 -0500

    Sad

--- VidTalk/settings/production.py
+++ VidTalk/settings/production.py
@@ -5,7 +5,7 @@ load_dotenv('.envprod')
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'FALSE').upper() == 'TRUE'
-ALLOWED_HOSTS = ['167.172.224.226']
+ALLOWED_HOSTS = ['167.172.224.226', 'https://167.172.224.226/', 'http://167.172.224.226/']
 
 DATABASES = {
     'default': {
