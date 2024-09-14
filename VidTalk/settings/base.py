b3799d87e100e7becfd7e728fec2dc20a0caf10d
Zachary Cherney <zach@Zacharys-MacBook-Pro.local>
2024-09-14 17:44:41 -0500

    Sighing rn

--- VidTalk/settings/base.py
+++ VidTalk/settings/base.py
@@ -1,19 +1,7 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key')
-
-DEBUG = False
-
-ALLOWED_HOSTS = []
-
-ROOT_URLCONF = 'VidTalk.urls'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -24,48 +12,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Main',
     'Dashboard',
-    # ... any other apps you've added
 ]
-
-# S3 Configuration
-USE_S3 = os.environ.get('USE_S3') == 'TRUE'
-
-if USE_S3:
-    # AWS settings
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-2')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    
-    # Remove ACL-related settings
-    AWS_DEFAULT_ACL = None
-    AWS_BUCKET_ACL = None
-    AWS_S3_FILE_OVERWRITE = False
-    
-    # S3 static settings
-    STATIC_LOCATION = 'static'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    
-    # S3 public media settings
-    PUBLIC_MEDIA_LOCATION = 'media'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-else:
-    STATIC_URL = '/static/'
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = BASE_DIR / 'media'
-
-DEFAULT_AUTO_Field = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'Main.CustomUser'
-LOGIN_REDIRECT_URL = 'dashboard'
-LOGOUT_REDIRECT_URL = 'index'
-LOGIN_URL = 'login'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -76,6 +23,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+ROOT_URLCONF = 'VidTalk.urls'
 
 TEMPLATES = [
     {
@@ -92,3 +41,10 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTH_USER_MODEL = 'Main.CustomUser'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'index'
+LOGIN_URL = 'login'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- VidTalk/settings/production.py
+++ VidTalk/settings/production.py
@@ -1,7 +1,10 @@
 from .base import *
+from dotenv import load_dotenv
 
-DEBUG = False
+load_dotenv('.envprod')
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+DEBUG = os.environ.get('DEBUG', 'FALSE').upper() == 'TRUE'
 ALLOWED_HOSTS = ['167.172.224.226']
 
 DATABASES = {
@@ -15,10 +18,29 @@ DATABASES = {
     }
 }
 
-# S3 settings
-USE_S3 = True
+USE_S3 = os.environ.get('USE_S3', 'FALSE').upper() == 'TRUE'
 
-# Security settings
-SECURE_SSL_REDIRECT = False  # Change this to True once you have HTTPS set up
-SESSION_COOKIE_SECURE = False  # Change this to True once you have HTTPS set up
-CSRF_COOKIE_SECURE = False  # Change this to True once you have HTTPS set up
+if USE_S3:
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+    AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_S3_CUSTOM_DOMAIN')
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    
+    STATIC_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
+
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
